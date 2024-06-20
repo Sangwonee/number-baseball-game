91257fa57e55a2e1c198c4e8cc7d795e1108d980
Sangwonee <111265742+Sangwonee@users.noreply.github.com>
2024-06-20 15:21:57 +0900

    숫자 야구 게임

--- src/NumberBaseballGame.java
+++ src/NumberBaseballGame.java
@@ -15,23 +15,18 @@ public class NumberBaseballGame {
         for (int num : comNumbersList) {
             comString += num;
         }
-        System.out.println("String으로 변환된  랜덤숫자 = " + comString);
-
 
         System.out.println("컴퓨터가 숫자를 생성하였습니다. 답을 맞춰보세요!");
 
         int attempts = 1;
 
         while (true) {
-            //사용자 입력 받기
             String input = scanner.nextLine();
             System.out.println(attempts + "번째 시도 : " + input);
 
-            //검증로직 (맞출때까지 반복해야 하니까)
             int strike = 0;
             int ball = 0;
 
-            // 스트라이크 검증로직
             for (int i = 0; i < input.length(); i++) {
                 if (comString.charAt(i) == input.charAt(i)) {
                     strike++;
@@ -52,7 +47,6 @@ public class NumberBaseballGame {
 
         }
 
-        // 종료 알림
         System.out.println(attempts + "번만에 맞추셨습니다.");
         System.out.println("게임을 종료합니다.");
 
